bdd25870a2583fa85b8a8d9fef5c61e78bc6b8c3
Albert Rayneer <rayneerqueiroz@gmail.com>
2020-08-11 09:09:10 -0300

    Initial Commit

---
+++ Freela/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Freela
+//
+//  Created by Albert Rayneer on 11/08/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
