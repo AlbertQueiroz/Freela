2960fe2e6abab779fc2c60dc0d1a519637713ab2
Albert Rayneer <rayneerqueiroz@gmail.com>
2020-08-11 09:17:11 -0300

    First commit

--- Freela/Controller/JobsListViewController.swift
+++ Freela/Controller/JobsListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  JobsListViewController.swift
 //  Freela
 //
 //  Created by Albert Rayneer on 11/08/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JobsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
 
 }
-
